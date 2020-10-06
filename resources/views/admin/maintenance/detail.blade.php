7aabf488adf891b895542d23e357463286d30382
Rajesh Solanki <rajesh.solanki195@gmail.com>
2020-10-06 16:11:23 +0530

    Maintenance module done

--- resources/views/admin/maintenance/detail.blade.php
+++ resources/views/admin/maintenance/detail.blade.php
@@ -21,7 +21,7 @@
                     <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                         <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                             <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}"><i class="fas fa-home"></i></a></li>
-                            <li class="breadcrumb-item active" aria-current="page"><a href="{{route('admin.maintenance.list')}}">Maintenance Guide</a></li>
+                            <li class="breadcrumb-item active" aria-current="page"><a href="{{route('admin.maintenance.list')}}">Maintenance Guides</a></li>
                             <li class="breadcrumb-item active" aria-current="page">{{$title}}</li>
                         </ol>
                     </nav>
