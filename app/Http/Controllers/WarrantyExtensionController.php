35ba6df4b915f0587bba14e600a29065c9cfe4df
Rajesh Solanki <rajesh.solanki195@gmail.com>
2020-10-09 11:37:17 +0530

    working on Warranty Extension

---
+++ app/Http/Controllers/WarrantyExtensionController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\WarrantyExtension;
+use Illuminate\Http\Request;
+use Storage;
+use Response;
+
+class WarrantyExtensionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('warranty_extension.add',array('title' => 'Warranty Extension'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\WarrantyExtension  $warrantyExtension
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WarrantyExtension $warrantyExtension)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\WarrantyExtension  $warrantyExtension
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WarrantyExtension $warrantyExtension)
+    {
+        return view('warranty_extension.add',array('title' => 'Warranty Extension','warranty'=>$warrantyExtension));
+    }
+
+    public function userImgUpload(Request $request, $id)
+    {
+        $file = $request->file('file');
+        if($file && $id){
+        
+            $request->validate([
+                'file' => 'mimes:jpeg,png,jpg,gif,svg|max:2048'
+            ]);
+            
+            $file_name =$file->getClientOriginalName();
+            $fileslug= pathinfo($file_name, PATHINFO_FILENAME);
+            $imageName = md5($fileslug.time());
+            $imgext =$file->getClientOriginalExtension();
+            $path = 'warranty_extension/'.$id.'/'.$imageName.".".$imgext;
+            $fileAdded = Storage::disk('public')->putFileAs('warranty_extension/'.$id.'/',$file,$imageName.".".$imgext);
+            
+            if($fileAdded){
+                $guideData = WarrantyExtension::find($id);
+                Storage::disk('public')->delete($guideData->picture_by_user);
+                $media = WarrantyExtension::where('id',$id)->update(['picture_by_user' => $path]);
+                return Response::json(['status' => true, 'message' => 'Media uploaded.']);
+            }
+            return Response::json(['status' => false, 'message' => 'Something went wrong.']);
+        }
+
+        return Response::json(['status' => false, 'message' => 'Something went wrong.']);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\WarrantyExtension  $warrantyExtension
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WarrantyExtension $warrantyExtension)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\WarrantyExtension  $warrantyExtension
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WarrantyExtension $warrantyExtension)
+    {
+        //
+    }
+}
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -38,10 +38,24 @@
             </li>
 
             <li class="nav-item">
-              <a class="nav-link {{ Request::routeIs('user.maintenance.*') ? 'active' : '' }}" href="{{route('user.maintenance.list')}}">
-                <i class="fas fa-toolbox text-red" aria-hidden="true"></i>
-                <span class="nav-link-text">Maintenance Guides</span>
+              <a class="nav-link {{ Request::routeIs('user.maintenance.*') || Request::routeIs('user.warranty_extension.*') ? 'active' : '' }}" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-examples">
+                <i class="fas fa-toolbox text-red"></i>
+                <span class="nav-link-text">Maintenance</span>
               </a>
+              <div class="collapse {{ Request::routeIs('user.maintenance.*')  || Request::routeIs('user.warranty_extension.*') ? 'show' : '' }}" id="navbar-examples">
+                <ul class="nav nav-sm flex-column">
+                  <li class="nav-item">
+                    <a href="{{route('user.maintenance.list')}}" class="nav-link {{ Request::routeIs('user.maintenance.list') ? 'active' : '' }}">
+                      <span class="sidenav-normal">Maintenance Guides </span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{route('user.warranty_extension.create')}}" class="nav-link {{ Request::routeIs('user.warranty_extension.create') ? 'active' : '' }}">
+                      <span class="sidenav-normal">Warranty Extension </span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </li>
             
             <li class="nav-item">
