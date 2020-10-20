5fdc4a144cfc8bd664d8b2214a690993b476e3fc
Rajesh Solanki <rajesh.solanki195@gmail.com>
2020-10-20 17:39:12 +0530

    minor chaanges

--- resources/views/admin/flowchart/preview.blade.php
+++ resources/views/admin/flowchart/preview.blade.php
@@ -20,6 +20,9 @@
                             </ol>
                         </nav>
                     </div>
+                    <div class="col-lg-6 col-5 text-right">
+                        <a href="{{route('admin.flowchart.list')}}" class="btn btn-sm btn-neutral">Back</a>
+                    </div>
                 </div>
             </div>
         </div>
